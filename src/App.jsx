24cc83d754c7c068f24a37c478cbac1503ac859d
esther-sh-choi <estherchoi.sh@gmail.com>
2022-06-16 02:16:28 -0400

    home page layout compatible with the drawer and the navbar

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,13 @@ import { createTheme } from "@mui/material/styles";
 import { ThemeProvider } from "@emotion/react";
 
 import Sidebar from "./components/sidebar/Sidebar";
-import Navbar from "./components/navbar/Navbar";
+
 import Home from "./pages/home/Home";
 import Login from "./pages/login/Login";
 import Eatlist from "./pages/eatlist/Eatlist";
 // import PersonalPage from "./pages/personalPage/PersonalPage";
+
+import "./app.scss";
 
 const theme = createTheme({
   palette: {
@@ -28,21 +30,10 @@ const theme = createTheme({
 function App() {
   const [mobileOpen, setMobileOpen] = useState();
 
-  const handleDrawerToggle = (mobileOpen) => {
-    setMobileOpen(mobileOpen);
-    console.log(mobileOpen);
-  };
-
   return (
     <ThemeProvider theme={theme}>
       <div className="app">
-        <div className="permanent">
-          <Sidebar
-            onSetMobileOpen={handleDrawerToggle}
-            mobileOpen={mobileOpen}
-          />
-          <Navbar onSetMobileOpen={handleDrawerToggle} />
-        </div>
+        <Sidebar />
         <Routes>
           <Route path="/">
             <Route index element={<Home />} />
